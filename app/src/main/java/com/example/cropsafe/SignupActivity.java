c2571688ba001328c78a8486c42de0f8ca9734aa
diwas <diwas.kuwar@gmail.com>
2023-11-20 13:47:25 +0545

    Nov 20

--- app/src/main/java/com/example/cropsafe/SignupActivity.java
+++ app/src/main/java/com/example/cropsafe/SignupActivity.java
@@ -1,7 +1,9 @@
 package com.example.cropsafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +12,22 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -21,11 +35,9 @@ public class SignupActivity extends AppCompatActivity {
 	private EditText editTextEmail;
 	private EditText editTextPassword;
 	private EditText editTextConfirmPassword;
-	private Button buttonSignUp;
+	private Button buttonLogin,buttonRedirect;
 
-	private static final String DB_URL = "jdbc:mysql://127.0.0.1:33036/cropsafe";
-	private static final String DB_USER = "root";
-	private static final String DB_PASSWORD = "";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -36,9 +48,16 @@ public class SignupActivity extends AppCompatActivity {
 		editTextEmail = findViewById(R.id.editTextEmail);
 		editTextPassword = findViewById(R.id.enterPassword);
 		editTextConfirmPassword = findViewById(R.id.reenterPassword);
-		buttonSignUp = findViewById(R.id.buttonSignUp);
+		buttonLogin = findViewById(R.id.buttonSignUp);
+		buttonRedirect=findViewById(R.id.loginRedirect);
 
-		buttonSignUp.setOnClickListener(new View.OnClickListener() {
+		buttonRedirect.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+			}
+		});
+		buttonLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				String name = editTextName.getText().toString();
@@ -50,9 +69,10 @@ public class SignupActivity extends AppCompatActivity {
 					Toast.makeText(SignupActivity.this, "Please fill all input fields", Toast.LENGTH_SHORT).show();
 				} else if (!password.equals(confirmPassword)) {
 					Toast.makeText(SignupActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-				} else if (!isValidEmail(email)) {
-					Toast.makeText(SignupActivity.this, "Incorrect email format", Toast.LENGTH_SHORT).show();
+				//} else if (!isValidEmail(email)) {
+				//	Toast.makeText(SignupActivity.this, "Incorrect email format", Toast.LENGTH_SHORT).show();
 				} else {
+				//	Log.d("DataSent", "Name: " + name + ", Email: " + email + ", Password: " + password);
 					// Perform registration process here (send data to PHP script)
 					performRegistration(name, email, password);
 				}
@@ -60,73 +80,64 @@ public class SignupActivity extends AppCompatActivity {
 		});
 	}
 
-	private boolean isValidEmail(String email) {
-		return Patterns.EMAIL_ADDRESS.matcher(email).matches();
-	}
-
 	private boolean performRegistration(String name, String email, String password) {
-		// Your registration logic here
 
-		Connection connection = null;
-		PreparedStatement preparedStatement = null;
+		String url = "http://172.17.18.189/cropsafe/sinup.php"; // Replace with your actual server URL
 
-		try {
-			// Establish a database connection
-			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			Toast.makeText(SignupActivity.this, "DB connected", Toast.LENGTH_SHORT).show();
-			// Check if the email already exists
-			if (emailExists(connection, email)) {
-				Toast.makeText(SignupActivity.this, "Email already registered", Toast.LENGTH_SHORT).show();
-				return false;
+		// Create a StringRequest to make a POST request
+		StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+				new Response.Listener<String>() {
+					@Override
+					public void onResponse(String response) {
+						// Handle the response from the server
+						try {
+							JSONObject jsonResponse = new JSONObject(response);
+							boolean error = jsonResponse.getBoolean("error");
+							String message = jsonResponse.getString("message");
+
+							if (!error) {
+								// Registration successful
+								Toast.makeText(SignupActivity.this, message, Toast.LENGTH_SHORT).show();
+							} else {
+								// Registration failed
+								Toast.makeText(SignupActivity.this, message, Toast.LENGTH_SHORT).show();
+							}
+						} catch (JSONException e) {
+							e.printStackTrace();
+							Toast.makeText(SignupActivity.this, "Error parsing JSON response", Toast.LENGTH_SHORT).show();
+						}
+					}
+				},
+				new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+						// Handle errors
+						Toast.makeText(SignupActivity.this, "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+						Log.d("error", "onErrorResponse: "+error.getMessage());
+					}
+				}) {
+			@Override
+			protected Map<String, String> getParams() {
+				// Set the parameters to be sent to the server
+				Map<String, String> params = new HashMap<>();
+				params.put("name", name);
+				params.put("email", email);
+				params.put("password", password);
+				return params;
 			}
+		};
+		int timeout = 30000; // 30 seconds
+		stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+				timeout,
+				DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+				DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+		));
 
-			// Insert user data into the database
-			String insertQuery = "INSERT INTO users_reg (name, email, password) VALUES (?, ?, ?)";
-			preparedStatement = connection.prepareStatement(insertQuery);
-			preparedStatement.setString(1, name);
-			preparedStatement.setString(2, email);
-			preparedStatement.setString(3, password);
-
-
-			int rowsInserted = preparedStatement.executeUpdate();
-			if (rowsInserted > 0) {
-				Toast.makeText(SignupActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-				return true;
-			} else {
-				Toast.makeText(SignupActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-				return false;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		} finally {
-			// Close resources
-			try {
-				if (preparedStatement != null) {
-					preparedStatement.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		// Add the request to the Volley request queue
+		Volley.newRequestQueue(this).add(stringRequest);
+		return true;
 	}
 
 
-	private static boolean emailExists(Connection connection, String email) throws SQLException {
-		PreparedStatement preparedStatement = null;
-		try {
-			String query = "SELECT email FROM users WHERE email = ?";
-			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, email);
-			return preparedStatement.executeQuery().next();
-		} finally {
-			if (preparedStatement != null) {
-				preparedStatement.close();
-			}
-		}
-	}
+
 }
-
